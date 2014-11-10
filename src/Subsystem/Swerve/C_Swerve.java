03f30d8af8e747e4241b5410a9f7c347c5f877ce
lcook1218 <1218@Dell-M4700-2.sch.org>
2014-11-10 10:20:29 -0500

    made robot rotate in corret direction

--- src/Subsystem/Swerve/C_Swerve.java
+++ src/Subsystem/Swerve/C_Swerve.java
@@ -4,6 +4,7 @@ import MathObject.O_Point;
 import Robot.CommandBase;
 import Robot.OI;
 import MathObject.O_Vector;
+import com.sun.squawk.util.MathUtils;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,21 +21,10 @@ public class C_Swerve extends CommandBase {
     }
 
     protected void initialize() {
-        //swerve.swerve(new O_Vector(-1, 0), new O_Point(0,0), 0.5f);
         
     }
 
     protected void execute() {
-/*
-        O_Vector vector = new O_Vector(-(float)OI.joystick1.getY(), (float)OI.joystick1.getX());
-        System.out.println("init vector:" + vector.description());
-        System.out.println("init vector angle:" + vector.getAngle());
-        vector.rotate90();
-        System.out.println("rotated 90:" + vector.description());
-        System.out.println("rotated vector angle:" + vector.getAngle());
-        vector.setMagnitude(1);
-        System.out.println("set magnitude:" + vector.description());
-        */
         if (!OI.leftThumbClick.get())
         {
             boolean shouldRunTwist = true;
@@ -53,19 +43,19 @@ public class C_Swerve extends CommandBase {
              if (OI.R1.get())//back right wheel
             {
                 System.out.println("turning of back right wheel");
-                swerve.swerve(new O_Vector(0, 0), new O_Point(-1,1), (float)OI.joystick1.getRawAxis(4));
+                swerve.swerve(new O_Vector(0, 0), new O_Point(-1,1), -1*(float)OI.joystick1.getRawAxis(4));
                shouldRunTwist = false;
             }
              if (OI.L1.get()) //back left wheel
             {
                 System.out.println("turning of back left wheel");
-                swerve.swerve(new O_Vector(0, 0), new O_Point(-1,-1), (float)OI.joystick1.getRawAxis(4));
+                swerve.swerve(new O_Vector(0, 0), new O_Point(-1,-1), -1*(float)OI.joystick1.getRawAxis(4));
                 shouldRunTwist = false;
             }
             if (shouldRunTwist)
             {
                 System.out.println("swerve twisting");
-                swerve.swerve(new O_Vector(-(float)OI.joystick1.getY(), (float)OI.joystick1.getX()), new O_Point(0,0), (float)OI.joystick1.getRawAxis(4));
+                swerve.swerve(new O_Vector(-(float)OI.joystick1.getY(), (float)OI.joystick1.getX()), new O_Point(0,0), -1*(float)OI.joystick1.getRawAxis(4));
             }
            }
         else
@@ -74,17 +64,22 @@ public class C_Swerve extends CommandBase {
             
                 //snake mode
             System.out.println("snake mode");
-                //swerve.swerve(new O_Vector(0, 0), new O_Point(1/((float)OI.joystick1.getRawAxis(4) + 0.02f ),0), (float)OI.joystick1.getY());
-          
-         }
-        
-        //swerve.swerve(new O_Vector(0, 0), new O_Point(0,0), (float)OI.joystick1.getRawAxis(4));
-       System.out.flush();
-       // System.out.println("Running C_Swerve");
-        //System.out.println("execting C_Swerve");
-        //swerve.swerve(90, (float)0.75, new O_Point(0,0), 0);
+            swerve.swerve(new O_Vector(0, 0), new O_Point(0, 1/((float)OI.joystick1.getRawAxis(4) + 0.02f )), (float)OI.joystick1.getY() * getSin(OI.joystick1.getRawAxis(4)));
+         
+        }
     }
 
+    float getSin(double value)
+    {
+        if (value >= 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
     protected boolean isFinished() {
         return false;
     }
